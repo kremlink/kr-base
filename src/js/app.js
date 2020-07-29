4cece004c727592bbd5111f6543d4cea8ef074b3
Sasha <kremlink0@gmail.com>
2020-07-29 14:51:29 +0300

    app.js minifix

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import {app} from './bf/base.js';
 import * as index from './modules/index/index.js';
 
 import {Toggle} from './bf/lib/toggle.js';
+//import {utils} from './bf/lib/utils.js';//or add it to app as follows
 //------------------------
 const modules=app.get('helpers.html').data('app').split(',');
 //------------------------
